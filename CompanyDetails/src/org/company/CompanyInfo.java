de5807807088d651b19f0916fcc40da9f2a3c001
sowndharya lakshmi <40987057+SowdharyaAspire@users.noreply.github.com>
2021-10-30 09:42:09 +0530

    Constructor Example Updation

--- CompanyDetails/src/org/company/CompanyInfo.java
+++ CompanyDetails/src/org/company/CompanyInfo.java
@@ -1,6 +1,12 @@
 package org.company;
 
 public class CompanyInfo {
+	public CompanyInfo(String Word) {
+		System.out.println(Word);
+		String word2 = " welcome to our company";
+		String word3 = Word.concat(word2);
+		System.out.println(word3);
+	}
 
 	public void companyName() {
 		System.out.println("Company Name is printed");
@@ -15,7 +21,7 @@ public class CompanyInfo {
 	}
 
 	public static void main(String[] args) {
-		CompanyInfo companyInfo = new CompanyInfo();
+		CompanyInfo companyInfo = new CompanyInfo("GreensTech!!!");
 		companyInfo.companyName();
 		companyInfo.companyId();
 		companyInfo.companyAddress();
